815cdac1987c6a30b130cd292550ca290997dfc4
afope2000 <afope@yahoo.com>
2022-09-12 09:16:50 -0700

    how to use puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - Entry point
+ *
+ *Description: 'Using puts to print text'
+ *
+ *Return: Always 0 (Succes)
+ */
+int main(void)
+{
+char strng1[50];
+strcpy(strng1, "Programming is like building a multilingual puzzle \n");
+puts(strng1);
+return (0);
+}
+
